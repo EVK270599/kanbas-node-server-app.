1651071f456f0ee4fae6dd07a0b53d8b86d464c7
Kaustubha Venkata Eluri <venkatakaustubha@gmail.com>
2024-12-08 17:45:40 -0800

    a6

--- Kanbas/Assignments/dao.js
+++ Kanbas/Assignments/dao.js
@@ -1,23 +1,18 @@
-import Database from "../Database/index.js";
-export function createAssignment(assignment) {
-  const newAssignment = { ...assignment, _id: Date.now().toString() };
-  Database.assignments = [...Database.assignments, newAssignment];
-  return newAssignment;
+import model from "./model.js";
+
+export const createAssignment = (assignment) => {
+  delete assignment._id;
+  return model.create(assignment);
 }
 
 export function deleteAssignment(assignmentId) {
-  const { assignments } = Database;
-  Database.assignments = assignments.filter((assignment) => assignment._id !== assignmentId);
+  return model.deleteOne({ _id: assignmentId });
  }
 
-export function updateAssignment(assignmentId, assignmentUpdates) {
-  const { assignments } = Database;
-  const assignment = assignments.find((assignment) => assignment._id === assignmentId);
-  Object.assign(assignment, assignmentUpdates);
-  return assignment;
+ export function updateAssignment(assignmentId, assignmentUpdates) {
+  return model.updateOne({ _id: assignmentId }, assignmentUpdates);
 }
 
-export function findAssignmentsForCourse(courseId) {
-  const { assignments } = Database;
-  return assignments.filter((assignment) => assignment.course === courseId);
+export const findAssignmentsForCourse =(courseId) =>{
+  return model.find({ course: courseId });
 }
